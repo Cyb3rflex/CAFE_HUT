6ff36a81f1f284a51e95283ef98b13b0cfa2d3b6
Abdulmuheez <abdulmuheezq@gmail.com>
2024-07-31 16:39:35 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+
+    navToggle.addEventListener('click', function () {
+        navMenu.classList.toggle('active');
+        navToggle.classList.toggle('active');
+    });
+});
